9e5a94239dd4400dbce6f48c2fd431bb16d80efe
DyBuu <victor.silva.m97@gmail.com>
2018-10-17 18:45:36 -0300

    Criando codigo da class retangulo

--- Figuras(class abstract)/retangulo.cs
+++ Figuras(class abstract)/retangulo.cs
@@ -1,1 +1,21 @@
+using System;
 
+namespace curso
+{
+    class Retangulo : Figura
+    {
+        public double largura {get; set;}
+        public double altura {get; set; }
+
+        public Retangulo (double largura, double altura)
+        {
+            this.largura = largura;
+            this.altura = altura;
+        }
+
+        public override double area()
+        {
+            return largura * altura;
+        }        
+    }
+}
