66f7a24a06154d7030492e1e600bc2104a8f842e
DyBuu <victor.silva.m97@gmail.com>
2018-10-17 18:48:54 -0300

    Criando codigo da class triangulo

--- Figuras(class abstract)/triangulo.cs
+++ Figuras(class abstract)/triangulo.cs
@@ -1,1 +1,22 @@
+namespace curso
+{
+    class triangulo : figura
+    {
+        public double a {get; set; }
+        public double b {get; set; }
+        public double c {get; set; }
 
+        public triangulo (double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public override double area()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
