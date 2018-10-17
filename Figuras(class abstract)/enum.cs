f2bb8037e5ab6d06004f3f8efd9a55bcf8bc6a14
DyBuu <victor.silva.m97@gmail.com>
2018-10-17 19:22:21 -0300

    Criando class do tipo enum

---
+++ Figuras(class abstract)/enum.cs
@@ -0,0 +1,18 @@
+namespace curso
+{
+    enum mes
+    {
+        janeiro,
+        fevereiro,
+        marco,
+        abril,
+        maio,
+        junho,
+        julho,
+        agosto,
+        setembro,
+        outubro,
+        novembro,
+        dezembro
+    }
+}
