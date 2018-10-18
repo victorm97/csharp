f7f33ca2ea9e4e31d1797ed391727f6577cd8110
DyBuu <victor.silva.m97@gmail.com>
2018-10-18 19:00:58 -0300

    Class exeption criada

--- Cliente/OperacaoExeption.cs
+++ Cliente/OperacaoExeption.cs
@@ -1,1 +1,12 @@
+using System;
 
+namespace curso
+{
+    class OperacaoExeption : Exeption
+    {
+        public OperacaoExeption (string msg) : base(msg)
+        {
+            
+        }
+    }
+}
