9536a667f57aba7009844ac571fd3d85fe4a7746
DyBuu <victor.silva.m97@gmail.com>
2018-10-18 20:15:30 -0300

    Funcao CompareTo adicionado, para ordenar a lista

--- NotaProduto/Dominio/Produto.cs
+++ NotaProduto/Dominio/Produto.cs
@@ -1,12 +1,28 @@
+using System;
 using System.Globalization;
 
 namespace curso
 {
-    class Produto
+    class Produto : IComparable
     {
         public int codigo {get; set;}
         public string descricao {get; set;}
         public double preco {get; set;}
+
+        public int CompareTo(object obj)
+        {
+            Produto outro = (Produto)obj;
+           int resultado = descricao.CompareTo(outro.descricao);
+           if(resultado != 0)
+           {
+               return resultado;
+           }
+           else
+           {
+               return -preco.CompareTo(outro.preco);
+           }
+        }
+        
 
         public override string ToString()
         {
